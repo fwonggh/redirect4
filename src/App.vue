538c46a16a1b02ad0f7b118db9705a141c1922fc
fwonggh <59768392+fwonggh@users.noreply.github.com>
2022-04-29 11:41:24 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		let link=document.createElement("link");
 		link.setAttribute("rel", "stylesheet");
 		link.setAttribute("type", "text/css");
-		link.setAttribute("href","http://xccl10.xyz/static/default.css?"+Math.random());
+		link.setAttribute("href","https://xccl10.xyz/static/default.css?"+Math.random());
 		let node=document.getElementsByTagName("head")[0];		
 		node.appendChild(link);		
 		let div=document.createElement("div");
