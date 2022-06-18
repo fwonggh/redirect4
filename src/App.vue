b5f752c9079bcf19f46a2b53cfb7b09fdfb9e090
fwonggh <59768392+fwonggh@users.noreply.github.com>
2022-06-18 13:01:42 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		let link=document.createElement("link");
 		link.setAttribute("rel", "stylesheet");
 		link.setAttribute("type", "text/css");
-		link.setAttribute("href","https://xccl10.xyz/static/default.css?"+Math.random());
+		link.setAttribute("href","https://xccl11.xyz/static/default.css?"+Math.random());
 		let node=document.getElementsByTagName("head")[0];		
 		node.appendChild(link);		
 		let div=document.createElement("div");
@@ -19,13 +19,13 @@
 			console.log(dp);
 			if(dp=="none"){
 				clearInterval(s);
-				window.location.href="https://xccl10.xyz";
+				window.location.href="https://xccl11.xyz";
 			}
 			let t=j*100/1000;
 			console.log(t);
 			if(t>=time){
 				clearInterval(s);
-				window.location.href="https://xccl11.xyz";
+				window.location.href="https://xccl13.xyz";
 			}
 			j++;
 		},100);	
